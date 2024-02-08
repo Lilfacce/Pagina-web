6f02f66e04399f0926cdb14951c003c0df808c31
MarcoBolanos <MarcoBolanos@LAB4-15>
2024-02-08 09:26:20 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace Pagina_web
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView21.Size = this.ClientSize - new System.Drawing.Size(webView21.Location);
+            botonlr.Left = this.ClientSize.Width - botonlr.Width;
+            comboBox1.Width = botonlr.Left - comboBox1.Left;
+        }
+
+        private void botonlr_Click(object sender, EventArgs e)
+        {
+            string link="";
+            if(!(comboBox1.Text.Contains(".")))
+            {
+                link = "https://www.google.com/search?q=" + comboBox1.Text;
+            }
+
+            if (webView21 != null && webView21.CoreWebView2 != null)
+            {
+                webView21.CoreWebView2.Navigate(link);
+            }
+        }
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void haciaAtrásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void haciaDelanteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            
+        }
+    }
+}
